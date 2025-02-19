8d12d02fc6e961f0f84d786a1d1398dca857428c
psuuc-rjnbarrientos <renzjordan0102@gmail.com>
2025-02-19 18:57:52 +0800

    Removed a directory

--- Activity5/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MyController;
-
-
-
-Route::get('/{operator}/{num1}/{num2}', [MyController::class, 'calculate']); //ito naman yung route, yung operator yun yung ipapasa nya dun sa MyController kung anong operator yung gagamitin, yung num1 and num2 naman yung ipapasa nyang input tas yung calculate yung yung name nung buong method dun sa may MyController
