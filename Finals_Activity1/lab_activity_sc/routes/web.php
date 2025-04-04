903d996a6abbe68acbeb758f9637665c01997e47
psuuc-rjnbarrientos <renzjordan0102@gmail.com>
2025-04-04 22:32:37 +0800

    Changed the folder name to Finals_Activity1

---
+++ Finals_Activity1/lab_activity_sc/routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LoginController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register.show');
+Route::post('/register', [RegisterController::class, 'register'])->name('register');
+
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login.show');
+Route::post('/login', [LoginController::class, 'login'])->name('login');
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
