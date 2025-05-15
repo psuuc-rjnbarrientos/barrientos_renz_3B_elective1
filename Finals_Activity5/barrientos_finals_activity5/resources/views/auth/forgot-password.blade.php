0316040d43a3f3cbdd8e79b97558477da23d9098
psuuc-rjnbarrientos <renzjordan0102@gmail.com>
2025-05-15 16:13:24 +0800

    Submission of Finalas Activity 5

--- Finals_Activity5/barrientos_finals_activity5/resources/views/auth/forgot-password.blade.php
+++ Finals_Activity5/barrientos_finals_activity5/resources/views/auth/forgot-password.blade.php
@@ -12,13 +12,26 @@
         <!-- Email Address -->
         <div>
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required
+                autofocus />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
+        <div class="flex items-center justify-between mt-4">
+            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+                href="{{ route('login') }}">
+                {{ __('Back to Login') }}
+            </a>
+
+            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+                href="{{ route('register') }}">
+                {{ __('Back to Register') }}
+            </a>
+        </div>
+
+        <div class="flex items-center justify-center mt-4">
             <x-primary-button>
-                {{ __('Email Password Reset Link') }}
+                {{ __('Send Password Reset Link') }}
             </x-primary-button>
         </div>
     </form>
